bd33f3830fb6aadaa7b2e552b6c51c4700abac6c
pavankalyan56789 <pavankalyan56789@gmail.com>
2024-02-27 21:26:52 -0700

    updated code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express, {request} from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import connection  from "./db.js";
+import route from "./routes/route.js";
 
 const app = express();
 
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", route);
 
 connection();
 
---
+++ routes/route.js
@@ -0,0 +1,9 @@
+import express from "express"
+import { addUser } from "../controllers/user-controller.js";
+
+
+const route = express.Router();
+
+route.post("/add", addUser);
+
+export default route;
